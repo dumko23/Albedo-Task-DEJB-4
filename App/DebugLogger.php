3f90e6d3e13431c4bb38b4ee6cea11abea8ac550
Dima Parkhomenko <parhomenko.dmit@gmail.com>
2022-07-25 10:50:05 +0300

    Created phpDocs for handler methods

--- App/DebugLogger.php
+++ App/DebugLogger.php
@@ -8,11 +8,23 @@ use Monolog\LogRecord;
 
 class DebugLogger extends AbstractProcessingHandler
 {
+    /**
+     * Inheriting parent construct logic to initialize handler
+     *
+     * @param int|string|Level $level - level of message significance
+     * @param bool $bubble
+     */
     public function __construct(int|string|Level $level = Level::Debug, bool $bubble = true)
     {
         parent::__construct($level, $bubble);
     }
 
+    /**
+     * Main method that describes the logic of logging performed by this handler
+     *
+     * @param LogRecord $record - message, content and extra data of log message
+     * @return void
+     */
     protected function write(LogRecord $record): void
     {
         echo $record->formatted;
